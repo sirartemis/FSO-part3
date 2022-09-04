b60785ee2424bf6b69cfb1ff7e25ae0a3fdad639
Artemis <sirartemis91@gmail.com>
2022-09-04 19:41:48 +0300

    uibuild

---
+++ frontend/src/services/persons.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const personsPath = '/api/persons'
+const baseUrl = `https://bitter-snow-9585.fly.dev${personsPath}`
+
+const getAll = () => {
+    return axios
+            .get(baseUrl)
+            .then(response => response.data)
+}
+
+const create = newPerson => {
+    return axios
+            .post(baseUrl, newPerson)
+            .then(response => response.data)
+}
+
+const update = (id, changedPerson) => {
+    return axios
+            .put(`${baseUrl}/${id}`, changedPerson)
+            .then(response => response.data)
+}
+
+const deletePerson = id => {
+    return axios
+            .delete(`${baseUrl}/${id}`)
+            .then(response => response.data)
+}
+
+export default {
+    getAll: getAll,
+    create: create,
+    update: update,
+    deletePerson: deletePerson
+}
